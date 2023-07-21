2eba7e84e062f8b944dc962142ea6d9e287f7986
Cheis202 <Cheis0.0Brock@gmail.com>
2023-07-21 10:34:25 -0300

    probando

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -6,24 +6,30 @@ const Contador = () => {
   const [counter, setCounter] = useState({dia: 13, hs:23, min:59 , sec:59})
   const masTiempo =() =>{
     setCounter(prev => ({...prev, sec:prev.sec--}))
-    if(counter.sec === 1){
-      setCounter(prev => ({...prev, min: prev.min-=1}))
-      setCounter(prev => ({...prev, sec:59 }))
-
-      if(counter.min === 0){
-        setCounter(prev => ({...prev, hs: prev.hs-=1}))
-        setCounter(prev => ({...prev, min:59 }))
-
-        if(counter.hs === 0){
-          setCounter(prev => ({...prev, dia: prev.dia-=1}))
-          setCounter(prev => ({...prev, hs:23 }))
+    try {
+      if(counter.sec === 1){
+        setCounter(prev => ({...prev, min: prev.min-=1}))
+        setCounter(prev => ({...prev, sec:59 }))
+  
+        if(counter.min === 0){
+          setCounter(prev => ({...prev, hs: prev.hs-=1}))
+          setCounter(prev => ({...prev, min:59 }))
+  
+          if(counter.hs === 0){
+            setCounter(prev => ({...prev, dia: prev.dia-=1}))
+            setCounter(prev => ({...prev, hs:23 }))
+          }
         }
       }
+    } catch (error) {
+      console.log(error)
     }
+    
   }
   setTimeout(masTiempo,1000)
 
     return(
+      
         <div className='counter'>
           <ContadorBox text={"DAYS"} time={counter.dia}/>
           <ContadorBox text={"HOURS"} time={counter.hs}/>
